6871d53b89d4a66b5f068c2c80ba19559c52a6f3
MichaelKingOcean <micheal.oshundaro7@gmail.com>
2016-11-07 11:44:17 +0000

    Show Message from input

---
+++ src/app/loop-back/loop-back.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'loop-back',
+  template: `
+    <input #box (keyup)="0">
+    <p>{{box.value}}</p>
+  `
+})
+export class LoopBackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
